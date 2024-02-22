02d6bb6495ad9a5d2dfc49ddca5858e9266c860a
Alexiotovv <gpsalexvasquez@gmail.com>
2024-02-22 13:54:27 -0500

    corrigiendo_assets

--- resources/views/bases/base.blade.php
+++ resources/views/bases/base.blade.php
@@ -8,24 +8,24 @@
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<!--favicon-->
-	<link rel="icon" href="../../../assets/images/favicon-32x32.png" type="image/png" />
+	<link rel="icon" href="{{ asset('assets/images/favicon-32x32.png')}}" type="image/png" />
 	<!--plugins-->
-	<link href="../../../assets/plugins/simplebar/css/simplebar.css" rel="stylesheet" />
-	<link href="../../../assets/plugins/perfect-scrollbar/css/perfect-scrollbar.css" rel="stylesheet" />
-	<link href="../../../assets/plugins/metismenu/css/metisMenu.min.css" rel="stylesheet" />
-	<link href="../../../assets/plugins/datatable/css/dataTables.bootstrap5.min.css" rel="stylesheet" />
+	<link href="{{asset('assets/plugins/simplebar/css/simplebar.css')}}" rel="stylesheet" />
+	<link href="{{asset('assets/plugins/perfect-scrollbar/css/perfect-scrollbar.css')}}" rel="stylesheet" />
+	<link href="{{asset('assets/plugins/metismenu/css/metisMenu.min.css')}}" rel="stylesheet" />
+	<link href="{{asset('assets/plugins/datatable/css/dataTables.bootstrap5.min.css')}}" rel="stylesheet" />
 	<!-- loader-->
-	<link href="../../../assets/css/pace.min.css" rel="stylesheet" />
-	<script src="../../../assets/js/pace.min.js"></script>
+	<link href="{{asset('assets/css/pace.min.css')}}" rel="stylesheet" />
+	<script src="{{asset('assets/js/pace.min.js')}}"></script>
 	<!-- Bootstrap CSS -->
-	<link href="../../../assets/css/bootstrap.min.css" rel="stylesheet">
-	<link href="../../../assets/css/bootstrap-extended.css" rel="stylesheet">
-	<link href="../../../assets/css/app.css" rel="stylesheet">
-	<link href="../../../assets/css/icons.css" rel="stylesheet">
+	<link href="{{asset('assets/css/bootstrap.min.css')}}" rel="stylesheet">
+	<link href="{{asset('assets/css/bootstrap-extended.css')}}" rel="stylesheet">
+	<link href="{{asset('assets/css/app.css')}}" rel="stylesheet">
+	<link href="{{asset('assets/css/icons.css')}}" rel="stylesheet">
 	<!-- Theme Style CSS -->
-	<link rel="stylesheet" href="../../../assets/css/dark-theme.css" />
-	<link rel="stylesheet" href="../../../assets/css/semi-dark.css" />
-	<link rel="stylesheet" href="../../../assets/css/header-colors.css" />
+	<link rel="stylesheet" href="{{asset('assets/css/dark-theme.css')}}" />
+	<link rel="stylesheet" href="{{asset('assets/css/semi-dark.css')}}" />
+	<link rel="stylesheet" href="{{asset('assets/css/header-colors.css')}}" />
 	<title>Syndron - Bootstrap5 Admin Template</title>
 </head>
 
@@ -36,7 +36,7 @@
 		<div class="sidebar-wrapper" data-simplebar="true">
 			<div class="sidebar-header">
 				<div>
-					<img src="../../../assets/images/logo-icon.png" class="logo-icon" alt="logo icon">
+					<img src="{{asset('assets/images/logo-icon.png')}}" class="logo-icon" alt="logo icon">
 				</div>
 				<div>
 					<h4 class="logo-text">Pueblos Originarios</h4>
@@ -275,7 +275,7 @@
 										<a class="dropdown-item" href="javascript:;">
 											{{-- <div class="d-flex align-items-center">
 												<div class="user-online">
-													<img src="../../../assets/images/avatars/avatar-1.png" class="msg-avatar" alt="user avatar">
+													<img src="{{asset('assets/images/avatars/avatar-1.png')}}" class="msg-avatar" alt="user avatar">
 												</div>
 												<div class="flex-grow-1">
 													<h6 class="msg-name">Daisy Anderson <span class="msg-time float-end">5 sec
@@ -287,7 +287,7 @@
 										<a class="dropdown-item" href="javascript:;">
 											{{-- <div class="d-flex align-items-center">
 												<div class="user-online">
-													<img src="../../../assets/images/avatars/avatar-2.png" class="msg-avatar" alt="user avatar">
+													<img src="{{asset('assets/images/avatars/avatar-2.png')}}" class="msg-avatar" alt="user avatar">
 												</div>
 												<div class="flex-grow-1">
 													<h6 class="msg-name">Althea Cabardo <span class="msg-time float-end">14
@@ -299,7 +299,7 @@
 										<a class="dropdown-item" href="javascript:;">
 											{{-- <div class="d-flex align-items-center">
 												<div class="user-online">
-													<img src="../../../assets/images/avatars/avatar-3.png" class="msg-avatar" alt="user avatar">
+													<img src="{{asset('assets/images/avatars/avatar-3.png')}}" class="msg-avatar" alt="user avatar">
 												</div>
 												<div class="flex-grow-1">
 													<h6 class="msg-name">Oscar Garner <span class="msg-time float-end">8 min
@@ -311,7 +311,7 @@
 										<a class="dropdown-item" href="javascript:;">
 											{{-- <div class="d-flex align-items-center">
 												<div class="user-online">
-													<img src="../../../assets/images/avatars/avatar-4.png" class="msg-avatar" alt="user avatar">
+													<img src="{{asset('assets/images/avatars/avatar-4.png')}}" class="msg-avatar" alt="user avatar">
 												</div>
 												<div class="flex-grow-1">
 													<h6 class="msg-name">Katherine Pechon <span class="msg-time float-end">15
@@ -323,7 +323,7 @@
 										<a class="dropdown-item" href="javascript:;">
 											{{-- <div class="d-flex align-items-center">
 												<div class="user-online">
-													<img src="../../../assets/images/avatars/avatar-5.png" class="msg-avatar" alt="user avatar">
+													<img src="{{asset('assets/images/avatars/avatar-5.png')}}" class="msg-avatar" alt="user avatar">
 												</div>
 												<div class="flex-grow-1">
 													<h6 class="msg-name">Amelia Doe <span class="msg-time float-end">22 min
@@ -335,7 +335,7 @@
 										<a class="dropdown-item" href="javascript:;">
 											{{-- <div class="d-flex align-items-center">
 												<div class="user-online">
-													<img src="../../../assets/images/avatars/avatar-6.png" class="msg-avatar" alt="user avatar">
+													<img src="{{asset('assets/images/avatars/avatar-6.png')}}" class="msg-avatar" alt="user avatar">
 												</div>
 												<div class="flex-grow-1">
 													<h6 class="msg-name">Cristina Jhons <span class="msg-time float-end">2 hrs
@@ -347,7 +347,7 @@
 										<a class="dropdown-item" href="javascript:;">
 											{{-- <div class="d-flex align-items-center">
 												<div class="user-online">
-													<img src="../../../assets/images/avatars/avatar-7.png" class="msg-avatar" alt="user avatar">
+													<img src="{{asset('assets/images/avatars/avatar-7.png')}}" class="msg-avatar" alt="user avatar">
 												</div>
 												<div class="flex-grow-1">
 													<h6 class="msg-name">James Caviness <span class="msg-time float-end">4 hrs
@@ -359,7 +359,7 @@
 										<a class="dropdown-item" href="javascript:;">
 											<div class="d-flex align-items-center">
 												<div class="user-online">
-													<img src="../../../assets/images/avatars/avatar-8.png" class="msg-avatar" alt="user avatar">
+													<img src="{{asset('assets/images/avatars/avatar-8.png')}}" class="msg-avatar" alt="user avatar">
 												</div>
 												<div class="flex-grow-1">
 													<h6 class="msg-name">Peter Costanzo <span class="msg-time float-end">6 hrs
@@ -371,7 +371,7 @@
 										<a class="dropdown-item" href="javascript:;">
 											<div class="d-flex align-items-center">
 												<div class="user-online">
-													<img src="../../../assets/images/avatars/avatar-9.png" class="msg-avatar" alt="user avatar">
+													<img src="{{asset('assets/images/avatars/avatar-9.png')}}" class="msg-avatar" alt="user avatar">
 												</div>
 												<div class="flex-grow-1">
 													<h6 class="msg-name">David Buckley <span class="msg-time float-end">2 hrs
@@ -383,7 +383,7 @@
 										<a class="dropdown-item" href="javascript:;">
 											<div class="d-flex align-items-center">
 												<div class="user-online">
-													<img src="../../../assets/images/avatars/avatar-10.png" class="msg-avatar" alt="user avatar">
+													<img src="{{asset('assets/images/avatars/avatar-10.png')}}" class="msg-avatar" alt="user avatar">
 												</div>
 												<div class="flex-grow-1">
 													<h6 class="msg-name">Thomas Wheeler <span class="msg-time float-end">2 days
@@ -395,7 +395,7 @@
 										<a class="dropdown-item" href="javascript:;">
 											<div class="d-flex align-items-center">
 												<div class="user-online">
-													<img src="../../../assets/images/avatars/avatar-11.png" class="msg-avatar" alt="user avatar">
+													<img src="{{asset('assets/images/avatars/avatar-11.png')}}" class="msg-avatar" alt="user avatar">
 												</div>
 												<div class="flex-grow-1">
 													<h6 class="msg-name">Johnny Seitz <span class="msg-time float-end">5 days
@@ -414,7 +414,7 @@
 					</div>
 					<div class="user-box dropdown">
 						<a class="d-flex align-items-center nav-link dropdown-toggle dropdown-toggle-nocaret" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-							<img src="../../../assets/images/avatars/avatar-2.png" class="user-img" alt="user avatar">
+							<img src="{{asset('assets/images/avatars/avatar-2.png')}}" class="user-img" alt="user avatar">
 							<div class="user-info ps-3">
 								<p class="user-name mb-0">Pauline Seitz</p>
 								<p class="designattion mb-0">Web Designer</p>
@@ -562,19 +562,19 @@
 
 	
 	<!-- Bootstrap JS -->
-	<script src="../../../assets/js/bootstrap.bundle.min.js"></script>
+	<script src="{{asset('assets/js/bootstrap.bundle.min.js')}}"></script>
 	<!--plugins-->
-	<script src="../../../assets/js/jquery.min.js"></script>
-	<script src="../../../assets/plugins/simplebar/js/simplebar.min.js"></script>
-	<script src="../../../assets/plugins/metismenu/js/metisMenu.min.js"></script>
-	<script src="../../../assets/plugins/perfect-scrollbar/js/perfect-scrollbar.js"></script>
-	<script src="../../../assets/plugins/apexcharts-bundle/js/apexcharts.min.js"></script>
-	<script src="../../../assets/plugins/datatable/js/jquery.dataTables.min.js"></script>
-	<script src="../../../assets/plugins/datatable/js/dataTables.bootstrap5.min.js"></script>
+	<script src="{{asset('assets/js/jquery.min.js')}}"></script>
+	<script src="{{asset('assets/plugins/simplebar/js/simplebar.min.js')}}"></script>
+	<script src="{{asset('assets/plugins/metismenu/js/metisMenu.min.js')}}"></script>
+	<script src="{{asset('assets/plugins/perfect-scrollbar/js/perfect-scrollbar.js')}}"></script>
+	<script src="{{asset('assets/plugins/apexcharts-bundle/js/apexcharts.min.js')}}"></script>
+	<script src="{{asset('assets/plugins/datatable/js/jquery.dataTables.min.js')}}"></script>
+	<script src="{{asset('assets/plugins/datatable/js/dataTables.bootstrap5.min.js')}}"></script>
 	
-	<script src="../../../assets/js/index.js"></script>
+	<script src="{{asset('assets/js/index.js')}}"></script>
 	<!--app JS-->
-	<script src="../../../assets/js/app.js"></script>
+	<script src="{{asset('assets/js/app.js')}}"></script>
 	
 </body>
 
